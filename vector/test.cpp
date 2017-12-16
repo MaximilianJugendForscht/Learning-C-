2d3cf02b78f985e665d433abb44572ad01c07943
MaximilianJugendForscht <wittmaxi@outlook.de>
2017-12-16 15:21:49 +0100

    Base is finished. Now lets do some debugging.

--- vector/test.cpp
+++ vector/test.cpp
@@ -4,4 +4,6 @@
 int main () {
 	vec::vector <int> vec1;
 	std::cout << "HELLO WORLD" << std::endl;
+	vec1.push_back (1);
+	std::cout << vec1[0] << std::endl;
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -7,11 +7,16 @@ namespace vec {
 		vector ();
 		~vector ();
 		bool push_back (T input);
-
+		void preallocate (int size);
+		T operator[](int index);
+		
 	private:
+		void preAllocate ();
+		int itemNumb = 0;
 		int size = 0;
 		int typeSize = 0;
-		int preallocSize = 0;
+		int preallocSize = 1;
+		T * data;
 	};
 
 	template <typename T>
@@ -21,6 +26,10 @@ namespace vec {
 
 	template <typename T>
 	bool vector<T>::push_back (T input) {
+		if (size == itemNumb) {
+			preAllocate();
+		}
+		itemNumb ++;
 
 	}
 
@@ -29,4 +38,20 @@ namespace vec {
 
 	}
 
+	template <typename T> 
+	void vector<T>::preallocate(int size) {
+		preallocSize = size;
+		if (preallocSize < 1) preallocSize = 1;
+	}
+
+	template <typename T>
+	void vector<T>::preAllocate () {
+		data = (T*) malloc (typeSize * preallocSize);
+		size += preallocSize;
+	}
+
+	template <typename T>
+	T vector<T>::operator[] (int index) {
+		return data [index * typeSize];
+	}
 }
