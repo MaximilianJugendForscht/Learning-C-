23c3a698e945a05828816161ffed67868da193e7
MaximilianJugendForscht <wittmaxi@outlook.de>
2017-12-16 12:45:04 +0100

    Added first vectors

---
+++ vector/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "vector.hpp"
+
+using namespace std;
+
+int main () {
+	vec::vector <int> vec1;
+}
---
+++ vector/vector.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace vec {
+	template <typename T>
+	class vector {
+	public:
+		~vector ();
+		push_back (T input);
+
+	private:
+	};
+
+	template <typename T>
+	vector::push_back (T input) {
+		int typeSize = sizeof (T);
+	}
+
+	template <typename T>
+	vector::~vector () {
+		
+	}
+
+}
